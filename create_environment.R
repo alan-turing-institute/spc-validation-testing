9eb04e28bc7ec786d55807fc4def8a9f83e5e380
HSalat <hadrien.salat@ucl.ac.uk>
2023-07-11 17:17:29 +0100

    test XAI

--- create_environment.R
+++ create_environment.R
@@ -1,5 +1,9 @@
 install.packages(c('gamlss','gamlss.tr','gamlss.dist','gamlss.add','fitdistrplus','univariateML','R.utils','ggplot2'))
 
+# Test XAI:
+install.packages('shapr')
+install.packages('xgboost')
+
 dir.create("Data/")
 dir.create("Data/dl")
 dir.create("Data/area")
