9fe6a64436d63bebc08ade745c4a4eb4bc4687fd
HSalat <hadrien.salat@ucl.ac.uk>
2023-07-31 19:11:26 +0100

    Preparing generalised version

--- clean-XAI.R
+++ clean-XAI.R
@@ -1,109 +1,129 @@
+library(igraph)
+library(readxl)
+library(geojsonR)
+library(geosphere)
+library(tidyr)
 
-### Label clustering. Possible labels:
-#     - [ ] Centrality measures within transportation network
-#     - [ ] “Geographic centrality” aka distance from highest density areas
-#     - [ ] Population density (better than categorical rural urban -- see def pb and World Pop 1000 inh./sqkm)
-#     - [ ] Age; ethnicity? (<- also pb is categorical; maybe keep numerical initially)
-#     - [ ] Index of deprivation
-#     - [ ] Life satisfaction?
-#
-# SUPPORTED MODELS: stats::lm; stats::glm; ranger::ranger; mgcv::gam; xgboost::xgb.train
-# Custom model possible, but implementation looks wonky
-
-# Note: categorical supported (see full doc online: https://cran.r-project.org/web/packages/shapr/vignettes/understanding_shapr.html), but more restrictive
-# Also, no NA
-
-## For initial analysis, let's compute for each MSOA:
+## Labels for each geography:
 #   - NUM: Closeness centrality from ONS OD matrix (Azure)
 #   - NUM: Betweenness centrality from ONS OD matrix (Azure)
 #   - NUM: Distance from nearest local high density area (?)
 #   - NUM: Population density (Compute from Shp)
 #   - NUM: Median age (Compute from SPC - Azure)
 #   - NUM: Index of deprivation (2019; https://www.gov.uk/government/statistics/english-indices-of-deprivation-2019)
-#
-# MODEL: xgboost (Ref: https://xgboost.readthedocs.io/en/stable/tutorials/model.html)
 
-## Define:
-#   - "Local high density area"
+### METHODS: xgboost, SHAP
 
-
-### METHODS:
-#     - SHAP <- Prob best?
-#             Four approaches: "empirical" (default), "gaussian", "copula", or "ctree" (gausian and copula require gaussian assumptions; only ctree for cat var; no independence assumption vs original kernel approach).
-#     - LIME <- Similar, less theoretically sound (supposedly)
-#     - TREPAN? (model specific)
+#TO DO:
+#   Check def of betweenness
+#   Check def of closeness
 
 
-#########################
-#########################
-####### Test data #######
-#########################
-#########################
+
+#############################
+#############################
+####### Get test data #######
+#############################
+#############################
 
 
-library(igraph)
-library(readxl)
-library(geojsonR)
-library(geosphere)
+downloadPrerequisites <- function(folderIn,fdl){
+  # LookUp table
+  if(!(file.exists(file.path(folderIn,fdl,"lookUp-GB.csv")))){
+    download.file("https://ramp0storage.blob.core.windows.net/referencedata/lookUp-GB.csv.gz",file.path(folderIn,fdl,"lookUp-GB.csv.gz"))
+    gunzip(file.path(folderIn,fdl,"lookUp-GB.csv.gz"))
+  }
+  # OD matrix
+  if(!(file.exists(file.path(folderIn,fdl,"commutingOD.csv")))){
+    download.file(url = "https://ramp0storage.blob.core.windows.net/nationaldata/commutingOD.gz", destfile = file.path(folderIn,fdl,"commutingOD.csv.gz"))
+    gunzip(file.path(folderIn,fdl,"commutingOD.csv.gz"))
+  }
+  # Pop at LSOA scale
+  if(!(file.exists(file.path(folderIn,fdl,"LSOA_2011_Pop20.geojson")))){
+    download.file("https://ramp0storage.blob.core.windows.net/nationaldata-v2/GIS/LSOA_2011_Pop20.geojson", destfile = file.path(folderIn,fdl,"LSOA_2011_Pop20.geojson"))
+  }
+  
+}
 
-# We use West Yorshire as a test area
-MSOAs_WY <- unique(lu$MSOA11CD[lu$AzureRef == "west-yorkshire"])
+loadPrerequisites <- function(folderIn,fdl){
+  # LookUp table
+  assign("lu", read.csv(file.path(folderIn,fdl,"lookUp-GB.csv")), envir = globalenv())
+  # Centrality from OD matrix as network
+  OD <- read.csv(file.path(folderIn,fdl,"commutingOD.csv"), header = T)
+  OD_wide <- pivot_wider(OD, names_from = HomeMSOA, values_from = Total_Flow)
+  OD_wide <- as.matrix(OD_wide)
+  OD_wide_prep <- OD_wide[,2:ncol(OD_wide)]
+  rownames(OD_wide_prep) <- OD_wide[,1]
+  OD_net <- graph_from_adjacency_matrix(OD_wide_prep, mode = "directed", weighted = TRUE, diag = TRUE)
+  assign("betweenness_global", betweenness(OD_net,directed = TRUE, normalized = TRUE),  envir = globalenv())
+  assign("closeness_global", closeness(OD_net, mode = "all", normalized = TRUE),  envir = globalenv())
+  # LSOA pop
+  lsoa_js <- FROM_GeoJson(url_file_string = file.path(folderIn,fdl,"LSOA_2011_Pop20.geojson"))
+  lsoa_feat <- lsoa_js$features
+  lsoa_pop <- rep(NA,length(lsoa_feat))
+  lsoa_names <- rep(NA,length(lsoa_feat))
+  for(i in 1:length(lsoa_feat)){
+    lsoa_pop[i] <- lsoa_feat[[i]]$properties$Pop20
+    lsoa_names[i] <- lsoa_feat[[i]]$properties$LSOA11CD
+  }
+  assign("pop_lsoa_global", data.frame(LSOA11CD = lsoa_names, pop = lsoa_pop),  envir = globalenv())
+}
 
-labels_WY <- data.frame(MSOA11CD = MSOAs_WY, closeness = NA, betweenness = NA, distHPD = NA,
-                        popDens = NA, medAge = NA, deprivation = NA)
+# For testing
+area_name = "west-yorkshire"
+scale = "LSOA11CD"
+scale = "LAD20CD"
 
-### OD matrix from Azure -> closeness and betweenness
-download.file(url = "https://ramp0storage.blob.core.windows.net/nationaldata/commutingOD.gz", destfile = file.path(folderIn,fdl,"commutingOD.csv.gz"))
-gunzip(file.path(folderIn,fdl,"commutingOD.csv.gz"))
-OD <- read.csv(file.path(folderIn,fdl,"commutingOD.csv"), header = T)
-
-OD_wide <- pivot_wider(OD, names_from = HomeMSOA, values_from = Total_Flow)
-OD_wide <- as.matrix(OD_wide)
-OD_wide_prep <- OD_wide[,2:ncol(OD_wide)]
-rownames(OD_wide_prep) <- OD_wide[,1]
-
-ncol(OD_wide_prep)
-nrow(OD_wide_prep)
-
-M <- matrix(0:8,ncol = 3, nrow = 3)
-M_net <- graph_from_adjacency_matrix(M, mode = "directed", weighted = TRUE, diag = TRUE)
-plot(M_net)
-
-OD_net <- graph_from_adjacency_matrix(OD_wide_prep, mode = "directed", weighted = TRUE, diag = TRUE)
-
-#plot(OD_net)
-dev.off()
-
-get.edgelist(OD_net)
-
-
-test <- betweenness(OD_net,directed = TRUE, normalized = TRUE)
-#test <- betweenness(OD_net,directed = TRUE, weights = , normalized = TRUE)
-between_WY <- test[which(names(test) %in% MSOAs_WY)]
-
-range(between_WY * 100000)
-
-# Arbitrary normalisation so that it looks nice
-for(i in 1:nrow(labels_WY)){
-  labels_WY$betweenness[i] <- between_WY[names(between_WY) == labels_WY$MSOA11CD[i]] * 100000
+# area_name must point to one of the preprocessed files on Azure
+# Min scale is LSOA due to source control data
+prepareLabels <- function(area_name,scale = c("LSOA11CD","MSOA11CD","LAD20CD"),median_age){
+  scale <- match.arg(scale)
+  ref <- which(lu$AzureRef == area_name)
+  lu_area <- data.frame(lu[ref,c("LSOA11CD","MSOA11CD","LAD20CD")])
+  lu_area <- lu_area[!duplicated(lu_area),]
+  lu_area <- merge(lu_area, pop_lsoa_global, by.x = "LSOA11CD", by.y = "LSOA11CD", all.x = T)
+  list_area <- unique(lu_area[,scale])
+  labels_area <- data.frame(area = list_area, closeness = NA, betweenness = NA, distHPD = NA,
+                          popDens = NA, medAge = NA, deprivation = NA)
+  colnames(labels_area)[1] <- scale
+  # Centrality
+  if(scale == "LSOA11CD"){
+    print("OD data is at MSOA scale, using parent MSOA")
+    for(i in 1:length(list_area)){
+      labels_area$betweenness[i] <- betweenness_global[names(betweenness_global) == lu_area$MSOA11CD[i]] * 100000
+      labels_area$closeness[i] <- closeness_global[names(closeness_global) == lu_area$MSOA11CD[i]] * 10
+    }
+  }else if(scale == "MSOA11CD"){
+    for(i in 1:length(list_area)){
+      labels_area$betweenness[i] <- betweenness_global[names(betweenness_global) == list_area[i]] * 100000
+      labels_area$closeness[i] <- closeness_global[names(closeness_global) == list_area[i]] * 10
+    }
+  }else if(scale == "LAD20CD"){
+    print("OD data is at MSOA scale, using population weighted averages")
+    for(i in 1:length(list_area)){
+      ref <- unique(lu_area$MSOA11CD[lu_area$LAD20CD == list_area[i]])
+      betw <- rep(NA,length(ref))
+      clos <- rep(NA,length(ref))
+      pops <- rep(NA,length(ref))
+      for(j in 1:length(ref)){
+        betw[j] <- betweenness_global[names(betweenness_global) == ref[j]] * 10000
+        clos[j] <- closeness_global[names(closeness_global) == ref[j]] * 10
+        pops[j] <- sum(lu_area$pop[lu_area$MSOA11CD == ref[j]])
+      }
+      labels_area$betweenness[i] <- sum(betw * pops) / sum(pops)
+      labels_area$closeness[i] <- sum(clos * pops) / sum(pops)
+    }
+  }
+  # Median age
+  labels_area$medAge <- median_age
 }
 
 
-test <- closeness(OD_net, mode = "all", normalized = TRUE)
-close_WY <- test[which(names(test) %in% MSOAs_WY)]
+# From global: lu, betweenness_global, closeness_global, pop_lsoa_global
 
-range(close_WY * 10)
-
+i = 1
 for(i in 1:nrow(labels_WY)){
-  labels_WY$closeness[i] <- close_WY[names(close_WY) == labels_WY$MSOA11CD[i]] * 10
-}
-
-
-### SPC output -> Median age
-
-i = 1 
-for(i in 1:nrow(labels_WY)){
-  labels_WY$medAge[i] <- median(dataWY$age[dataWY$MSOA11CD == labels_WY$MSOA11CD[i]])
+  median_age[i] <- median(dataWY$age[dataWY$MSOA11CD == labels_WY$MSOA11CD[i]])
 }
 
 ### Shapefile -> pop density and distance to nearest highest density area
@@ -292,19 +312,17 @@ testWY_sal <- moments("incomeH",dataWYnotNA,scale = "MSOA11CD",lu)
 # Check MSOAs are ordered properlyt
 labels_WY$MSOA11CD == testWY$area
 
-test <- sample(1:nrow(testWY_sal), 20)
-
 # Training set
-x_train <- as.matrix(labels_WY[-test,2:7])
+x_train <- as.matrix(labels_WY[-1:-20,2:7])
 
 # Results: Order 1-4: mean, sd, skewness, kurtosis
-y_train_1 <- testWY_sal[-test, "mean"]
-y_train_2 <- testWY_sal[-test, "sd"]
-y_train_3 <- testWY_sal[-test, "skewness"]
-y_train_4 <- testWY_sal[-test, "kurtosis"]
+y_train_1 <- testWY_sal[-1:-20, "mean"]
+y_train_2 <- testWY_sal[-1:-20, "sd"]
+y_train_3 <- testWY_sal[-1:-20, "skewness"]
+y_train_4 <- testWY_sal[-1:-20, "kurtosis"]
 
 # Test set
-x_test <- as.matrix(labels_WY[test, 2:7])
+x_test <- as.matrix(labels_WY[1:20, 2:7])
 
 ### Mean
 
@@ -378,7 +396,6 @@ print(explanation_1$dt)
 # Plot the resulting explanations for observations 1 and 6
 plot(explanation_1, plot_phi0 = FALSE, index_x_test = c(1:4))
 
-### Plot: absolute impact (fairly useless)
 avg1 <- colSums(abs(explanation_1$dt[,2:7]))/20
 avg2 <- colSums(abs(explanation_2$dt[,2:7]))/20
 avg3 <- colSums(abs(explanation_3$dt[,2:7]))/20
@@ -388,45 +405,15 @@ res <- rbind(avg1,avg2,avg3,avg4)
 row.names(res) <- c("Mean", "Sd", "Skewness", "Kurotsis")
 res <- t(res)
 
-image(1:ncol(res), 1:nrow(res), t(res), axes = FALSE, main = "West Yorshire salaries; absolute importance")
-axis(1, 1:ncol(res), colnames(res))
-axis(2, 1:nrow(res), rownames(res))
-
-### Plot without abs
-avg1 <- colSums(explanation_1$dt[,2:7])/20
-avg2 <- colSums(explanation_2$dt[,2:7])/20
-avg3 <- colSums(explanation_3$dt[,2:7])/20
-avg4 <- colSums(explanation_4$dt[,2:7])/20
-
-res <- rbind(avg1,avg2,avg3,avg4)
-row.names(res) <- c("Mean", "Sd", "Skewness", "Kurotsis")
-res <- t(res)
-
-image(1:ncol(res), 1:nrow(res), t(res), axes = FALSE, col = hcl.colors(12, "Plasma", rev = TRUE),
-      main = "West Yorshire salaries; average importance")
-axis(1, 1:ncol(res), colnames(res))
-axis(2, 1:nrow(res), rownames(res))
-
-
-### Plot without abs and norm by feature
-avg1 <- colSums(explanation_1$dt[,2:7])/20
-avg2 <- colSums(explanation_2$dt[,2:7])/20
-avg3 <- colSums(explanation_3$dt[,2:7])/20
-avg4 <- colSums(explanation_4$dt[,2:7])/20
-
-res <- rbind(avg1,avg2,avg3,avg4)
-row.names(res) <- c("Mean", "Sd", "Skewness", "Kurotsis")
-res <- t(res)
-
 for(i in 1:4){
   res[,i] <- res[,i]/max(res[,i])
 }
 
-image(1:ncol(res), 1:nrow(res), t(res), axes = FALSE, col = hcl.colors(12, "Plasma", rev = TRUE),
-      main = "West Yorshire salaries; average importance normalised per column")
+image(1:ncol(res), 1:nrow(res), t(res), axes = FALSE, main = "West Yorshire salaries; relative importance norm. per column")
 axis(1, 1:ncol(res), colnames(res))
 axis(2, 1:nrow(res), rownames(res))
 
+image(res, axes = FALSE)
 
 
 
